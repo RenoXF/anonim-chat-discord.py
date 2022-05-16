262fec967b8f6595a8226af24348b4d4b57a6b31
iamRAF <renoxf11@gmail.com>
2022-05-16 18:30:19 +0700

    Update bot.py
    send image with message

--- bot.py
+++ bot.py
@@ -2,10 +2,11 @@ import discord
 
 client = discord.Client()
 
+
 @client.event
 async def on_message(message):
     empty_array = []
-    modmail_channel = discord.utils.get(client.get_all_channels(), name="modmail")
+    modmail_channel = discord.utils.get(client.get_all_channels(), name="jembut")
 
     if message.author == client.user:
         return
@@ -19,11 +20,14 @@ async def on_message(message):
         else:
             await modmail_channel.send("[" + message.author.display_name + "] " + message.content)
 
-    elif str(message.channel) == "modmail" and message.content.startswith("<"):
+    elif str(message.channel) == "jembut" and message.content.startswith("<"):
         member_object = message.mentions[0]
+        index = message.content.index(" ")
+        string = message.content
+        mod_message = string[index:]
         if message.attachments != empty_array:
             files = message.attachments
-            await member_object.send("[Anonim Chat]")
+            await member_object.send(" " + mod_message)
 
             for file in files:
                 await member_object.send(file.url)
@@ -31,7 +35,6 @@ async def on_message(message):
             index = message.content.index(" ")
             string = message.content
             mod_message = string[index:]
-            await member_object.send("" + mod_message)
-
+            await member_object.send(" " + mod_message)
 
 client.run('ODg3NTk0MTc3Njc4OTQyMjc4.GXSTyC.JlzF8k3B1eui6ICf2kJJvBrzzDZD8Isa4l63Kk')
